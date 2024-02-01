29c532f7533ea582268122513587b9c7fb0daf4b
Nahum Shalman <nshalman@equinix.com>
2024-02-01 15:35:28 +0000

    generate a traceparent string from the current context

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,26 @@ pub fn start_with_traceparent(name: &'static str) -> opentelemetry::ContextGuard
     opentelemetry::trace::mark_span_as_active(span)
 }
 
+/// Generate a traceparent string for propagation
+/// Reference: [https://github.com/open-telemetry/opentelemetry-rust/blob/0101233973ca8d635970bf7231c7eccda0e9764e/opentelemetry-sdk/src/propagation/trace_context.rs#L116-L123]
+pub fn generate_traceparent() -> Option<String> {
+    // https://github.com/open-telemetry/opentelemetry-rust/blob/0101233973ca8d635970bf7231c7eccda0e9764e/opentelemetry-sdk/src/propagation/trace_context.rs#L28C1-L29C1
+    const SUPPORTED_VERSION: u8 = 0;
+    return opentelemetry::trace::get_active_span(|span| {
+        let span_context = span.span_context();
+        if span_context.is_valid() {
+            return Some(format!(
+                "{:02x}-{}-{}-{:02x}",
+                SUPPORTED_VERSION,
+                span_context.trace_id(),
+                span_context.span_id(),
+                span_context.trace_flags() & opentelemetry::trace::TraceFlags::SAMPLED
+            ));
+        }
+        None
+    });
+}
+
 /// A super-duper opinionated way to initialize otel tracing.
 /// XXX currently hardcoding the OTLP endpoint which is wrong.
 pub fn init(name: &'static str) -> Result<(), Box<dyn std::error::Error>> {
